9330f3458819596465586bdefb28420f77fc740e
ben <benbout62@gmail.com>
2017-07-22 15:22:14 +0200

    SampQueryResult file was missing in the last commits

---
+++ SampQueryService/QueryResult/SampQueryResult.cs
@@ -0,0 +1,10 @@
+﻿namespace SampQueryService.QueryResult
+{
+    public abstract class SampQueryResult : IQueryDataResult
+    {
+        public char OpCode { get; internal set; }
+        public bool IsCompleted { get; internal set; }
+
+        public abstract void Deserialize(byte[] data);
+    }
+}
